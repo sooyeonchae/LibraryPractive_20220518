a4128d0c81506eff4660bd4afd29c094cfc5cc88
채수연 <sooyeonchae@gmail.com>
2022-05-18 14:46:27 +0900

    별도의 사진 크게보기 화면 구성 - zoom in / zoom out 구현 필요

---
+++ app/src/main/java/net/soozip/librarypractive_20220518/ViewPhotoActivity.kt
@@ -0,0 +1,11 @@
+package net.soozip.librarypractive_20220518
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+    }
+}
